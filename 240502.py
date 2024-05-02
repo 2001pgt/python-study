14e6ed544fd1e466dfb04a204a7be5ac60487e00
2001pgt <wldnro4210@naver.com>
2024-05-02 21:21:36 +0900

    문제를 풀려고했지만 결국 풀지못하고 시간이 지나 버렸다. 내일 이어서 풀도록하겠다.

--- 240502.py
+++ 240502.py
@@ -8,3 +8,17 @@
 # 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
 
 #나의 풀이
+a = input()
+word = a.lower()
+
+#문자 i와 나머지 문자들을 모두 비교하여 같은 것이 나올 때마다count+1을 한다 이렇게 모든 문자들을 비교하여 count가 가장 높은 문자를 출력하게 한다
+word_count = 0
+max_count = 0
+for i in range(len(word)):
+    for i2 in range(len(word)):
+        if word[i] == word[i2]:
+            word_count += 1
+    if max_count < word_count:
+        max_count = word_count
+    
+    
