fa1177521e6380b706860ec8b6fefb121a95d446
2001pgt <wldnro4210@naver.com>
2024-07-24 08:33:47 +0900

    소수 구하기 어제 만들었던 소수 판별 함수를 이용하면 쉽게 풀수있다.

--- 24.07/240712.py
+++ 24.07/240712.py
@@ -7,3 +7,27 @@
 
 # 출력
 # 한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+
+# 풀이
+# 입력
+import sys
+input = sys.stdin.readline
+
+M,N = map(int,input().split())
+
+# 어제 사용했던 소수 판별함수를 이용하여 소수를 판별
+def Check_sosu(x):
+    if x < 2:  # 0과 1은 소수가 아님
+        return False
+    for i in range(2,int(x**0.5)+1):
+        if x%i==0:
+            return False
+    return True
+
+for i in range(M,N+1):
+    # 출력
+    if Check_sosu(i) == True: # 소수가 맞으면
+        print(i) # 출력
+    # 아니면
+    else:
+        pass # 통과
