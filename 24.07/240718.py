1180b63285ff5d2a94b89e049d5f246ca1a89b38
2001pgt <wldnro4210@naver.com>
2024-08-09 05:43:51 +0900

    괄호

--- 24.07/240718.py
+++ 24.07/240718.py
@@ -11,3 +11,20 @@
 # 출력은 표준 출력을 사용한다. 만일 입력 괄호 문자열이 올바른 괄호 문자열(VPS)이면 “YES”, 아니면 “NO”를 한 줄에 하나씩 차례대로 출력해야 한다. 
 
 # 풀이
+# 입력
+import sys
+input = sys.stdin.readline
+
+T = int(input()) # 테스트 횟수
+def Check_VPS():
+    pass 
+for _ in range(T):
+    vps = list(input().strip().split(''))
+
+
+
+
+    if Check_VPS:
+        print("YES")
+    else:
+        print("NO")
