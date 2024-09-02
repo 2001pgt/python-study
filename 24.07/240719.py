9665b6a3e32864423b411e67bd4e9982ea4dcbdf
2001pgt <wldnro4210@naver.com>
2024-09-02 11:27:41 +0900

    균형잡힌 세상

--- 24.07/240719.py
+++ 24.07/240719.py
@@ -19,3 +19,65 @@
 # 입력의 종료조건으로 맨 마지막에 온점 하나(".")가 들어온다.
 # 출력
 # 각 줄마다 해당 문자열이 균형을 이루고 있으면 "yes"를, 아니면 "no"를 출력한다.
+
+import sys
+input = sys.stdin.readline
+
+# 스택 구현
+def add_stack(stack,x):
+    stack.append(x)
+def pop(stack):
+    if len(stack) == 0:
+        return print(-1)
+    else:
+        stack.pop()
+def checkstack(stack):
+    if len(stack) == 0:
+        return -1
+    else:
+        return stack[-1]
+
+# 스택을 활용하여 괄호가 완성되면 팝한다
+# 구현
+# 입력 받은 괄호를 스택형태로 저장한다
+# 만약 () 형태가 완성되면 2개를 팝한다
+# 그리고 최종적으로 스택에 남은 괄호가 있다면 vps가 아니다
+# 스택에 남은 괄호가 없다면 vps이다
+# ++++++++++++++++++++++++++++++++++++++++++
+# 문자열을 받으면 해당 문자열에서 괄호만 추출해야한다
+
+# 괄호를 추출하는 함수
+def choice(str):
+    ghalho = []
+    for i in range(len(str)):
+        if str[i] == "(" or str[i] == ")" or str[i] == "[" or str[i] == "]": 
+            ghalho.append(str[i])
+    return ghalho
+
+# 구현부
+while(True):
+    stack = []
+    # 입력
+    str = input().rstrip()
+    # 종료 조건
+    if str == ".":
+        break
+    # 괄호 추출
+    check_vps = choice(str)
+    # print(check_vps)
+    # print(check_vps[0])
+    for i in range(len(check_vps)):
+        add_stack(stack,check_vps[i])
+        # 스택에서 ) 가 들어왔을때 이전에 ( 이 있다면 2개 다 pop 한다.
+        if checkstack(stack) == ")"  and len(stack) > 1 and stack[-2] == "(": 
+            pop(stack)
+            pop(stack)
+        elif checkstack(stack) == "]" and len(stack) > 1 and stack[-2] == "[":
+            pop(stack)
+            pop(stack)
+        # print(f'현재 스택{stack}')  
+    # print(f'결과 스택{stack}')    
+    if len(stack) > 0:
+        print("no")
+    else:
+        print("yes")
